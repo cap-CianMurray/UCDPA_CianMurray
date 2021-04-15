709155cfe88e424d065f319f2f4a3c0f6c304190
CianMurray <cian.murray@capspire.com>
2021-04-15 08:51:29 +0100

    "First Commit: Git Interface Test"

---
+++ Cryptocurrency Analysis.py
@@ -0,0 +1,11 @@
+# Certificate in Data Analytics for Finance
+# Cian Murray cian.murray@capspire.com
+# 15/04/2021
+# Datasets Urls:
+    #https://data.world/pmohun/complete-historical-cryptocurrency-financial-data
+    #https://datahub.io/core/gold-prices
+
+
+
+
+
